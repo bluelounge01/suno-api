c2f603314d701dbd676e16105a495754dd01a62d
bluelounge <84225082+bluelounge01@users.noreply.github.com>
2025-06-11 21:29:24 +0900

    chore: avoid bundling playwright

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
+  webpack: (config, { isServer }) => {
     config.module.rules.push({
       test: /\.(ttf|html)$/i,
       type: 'asset/resource'
     });
+    if (isServer) {
+      const externals = [
+        'rebrowser-playwright-core',
+        'ghost-cursor-playwright',
+        '@playwright/browser-chromium',
+        'chromium-bidi',
+        'electron',
+        'bufferutil',
+        'utf-8-validate'
+      ];
+      config.externals = [...config.externals, ...externals];
+    }
     return config;
   },
   experimental: {
-    serverMinification: false, // the server minification unfortunately breaks the selector class names
-  },
-};  
+    serverMinification: false // the server minification unfortunately breaks the selector class names
+  }
+};
 
 export default nextConfig;
